ff8d1688e7cf2b20135ffcb34531653955f02fb0
Asad Shafiq <masadali5433@gmail.com>
2023-08-06 08:32:50 +0500

    changed mobile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,43 +3,126 @@ import { FaPhoneVolume } from "react-icons/fa";
 import { BsFillCartFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import logo from "../assets/logo-dark.png";
-import { useRef } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Header = () => {
-  const toggleRef = useRef();
-
+  const [isVisible, setIsVisible] = useState(false);
   const toggleNav = () => {
-    console.log("clicked");
-    const bool = toggleRef.current.getAttribute("data-visible");
-    console.log(bool);
-    if (bool === false || bool === "false")
-      toggleRef.current.setAttribute("data-visible", true);
-    else toggleRef.current.setAttribute("data-visible", false);
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 710) setIsVisible(true);
+      else setIsVisible(false);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const navVars = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        ease: [0.12, 0, 0.39, 0],
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  };
+
+  const navLinkVars = {
+    initial: {
+      y: "30vh",
+      transition: {
+        duration: 0.5,
+        ease: [0.37, 0, 0.63, 1],
+      },
+    },
+    open: {
+      y: 0,
+      transition: {
+        duration: 0.7,
+        ease: [0, 0.55, 0.45, 1],
+      },
+    },
+  };
+
+  const containerVars = {
+    initial: {
+      transition: {
+        staggerChildren: 0.09,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.09,
+        staggerDirection: 1,
+      },
+    },
   };
 
   return (
     <header className="header">
-      <nav
-        id="primary-nav"
-        className="primary-nav"
-        data-visible="false"
-        ref={toggleRef}
-      >
-        <ul className="primary-nav__list">
-          <li className="primary-nav__item">
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li className="primary-nav__item">
-            <Link to={"/"}>Menu</Link>
-          </li>
-          <li className="primary-nav__item">
-            <Link to={"/"}>Blog</Link>
-          </li>
-          <li className="primary-nav__item">
-            <Link to={"/"}>Shop</Link>
-          </li>
-        </ul>
-      </nav>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.nav
+            variants={navVars}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            id="primary-nav"
+            className="primary-nav"
+            data-visible="false"
+          >
+            <motion.ul
+              variants={containerVars}
+              initial="initial"
+              animate="open"
+              exit="initial"
+              className="primary-nav__list"
+            >
+              <div className="overflow-hidden">
+                <motion.li variants={navLinkVars} className="primary-nav__item">
+                  <Link to={"/"}>Home</Link>
+                </motion.li>
+              </div>
+              <div className="overflow-hidden">
+                <motion.li variants={navLinkVars} className="primary-nav__item">
+                  <Link to={"/"}>Menu</Link>
+                </motion.li>
+              </div>
+              <div className="overflow-hidden">
+                <motion.li variants={navLinkVars} className="primary-nav__item">
+                  <Link to={"/"}>Blog</Link>
+                </motion.li>
+              </div>
+              <div className="overflow-hidden">
+                <motion.li variants={navLinkVars} className="primary-nav__item">
+                  <Link to={"/"}>Shop</Link>
+                </motion.li>
+              </div>
+            </motion.ul>
+          </motion.nav>
+        )}
+      </AnimatePresence>
 
       <div className="header__logo-box">
         {/* TODO: Add your own logo */}
